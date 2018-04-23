6b2bb15d95180aa9f2a9eb1bb5ba793a6515effc
Matic Jesenicnik <matic.jesenicnik@gmail.com>
2018-04-23 19:29:18 +0200

    Database

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'header.php' ?>
+<?php include_once 'header.php' ?>
 <body class="">
   <!-- Pushy Menu -->
   <nav class="pushy pushy-left">
@@ -9,6 +9,7 @@
         <li><a href="#turnirji">Turnirji</a></li>
         <li><a href="#galerija">Galerija</a></li>
         <li><a href="#kontakt">Stopite v kontakt!</a></li>
+        <li><a href="register.php">Registracija</a></li>
     </ul>
   </nav>
 
@@ -59,44 +60,4 @@
             </div>
         </div>
     </section>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-8">
-                    <h3>Your Logo</h3>
-                    <p>© 2016 Your Company. Designed and Developed by <a href="http://www.themeinthebox.com">ThemeintheBox.com</a></p>
-                </div>
-                <div class="col-md-4">
-                    <p class="text-right social"><i class="typcn typcn-social-facebook-circular"></i><i class="typcn typcn-social-twitter-circular"></i><i class="typcn typcn-social-tumbler-circular"></i><i class="typcn typcn-social-github-circular"></i><i class="typcn typcn-social-dribbble-circular"></i></p>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Bootstrap core JavaScript
-================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/js/bootstrap-scrollspy.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-    <script src="http://masonry.desandro.com/masonry.pkgd.js"></script>
-    <script src="assets/js/masonry.js"></script>
-    <script src="assets/js/pushy.min.js"></script>
-    <script src="assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="assets/js/wow.min.js"></script>
-    <script src="assets/js/scripts.js"></script>
-    <script src="assets/js/odometer.js"></script>
-</body>
-</html>
-
-<script>
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-34344036-1', 'auto');
-ga('send', 'pageview');
-
-</script>
+<?php include_once 'footer.php' ?>
